ffc49909880d6ec204834623a3565c4e7f70d428
Flen-E <cjj642@naver.com>
2023-09-17 21:47:43 +0900

    install axios

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,13 +1,49 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components"
+
+const CoinList = styled.ul``;
+
+const Coin = styled.li``;
+
+const Title = styled.h1`
+    color : ${(props) => props.theme.accentColor};
+`;
+
+interface ICoin {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+}
+
 function Coins() {
-
+    const [coins , setCoins] = useState<ICoin[]>([]);
+    useEffect(()=>{
+        (async() =>{
+            const response = await (await fetch("https://api.coinpaprika.com/v1/coins")).json();
+            setCoins(response.slice(0,50));
+            console.log(setCoins);
+        })();
+    }, []);
+    
     return (
-        <div>
-            <h1>Coins</h1>
-            <h1>Coins</h1>
+        <>
+            <Title>코인</Title>
+            <CoinList>
+                {coins.map((coin) =>(
+                    <Coin key={coin.id}>
+                        <Link to={`/${coin.id}`}>
+                            {coin.name} &rarr;
 
-        </div>
-
+                        </Link>
+                    </Coin>
+                ))}
+            </CoinList>
+        </>
     )
-
 }
 export default Coins;
